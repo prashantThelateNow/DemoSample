2edb1f57465fb344bfbe1b6d04d1f4398da23794
prashantThelateNow <55628792+prashantThelateNow@users.noreply.github.com>
2023-10-19 15:17:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  // distDir: "build" // Optional: Change the output directory `out` -> `build`
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+// const nextConfig = {
+//   output: "export",
+//   // distDir: "build" // Optional: Change the output directory `out` -> `build`
+// };
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      
+    };
+  }
+
+  return {
+    output: "export",
+  };
 };
 
-module.exports = nextConfig;
